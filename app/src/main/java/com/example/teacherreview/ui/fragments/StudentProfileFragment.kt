7d3c975f23f56b60196bf3ba5c30c860370eb3da
TheProgrammerGod <suvo.ghosh.965@gmail.com>
2022-12-30 15:53:32 +0530

    Made the UI of profile page

--- app/src/main/java/com/example/teacherreview/ui/fragments/StudentProfileFragment.kt
+++ app/src/main/java/com/example/teacherreview/ui/fragments/StudentProfileFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.teacherreview.databinding.FragmentStudentProfileBinding
+import com.example.teacherreview.viewmodels.SharedViewModel
 
 
 class StudentProfileFragment : Fragment() {
     private lateinit var binding: FragmentStudentProfileBinding
+    // Creating the SharedViewModel Instance
+    private val sharedViewModel : SharedViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +22,11 @@ class StudentProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentStudentProfileBinding.inflate(inflater , container , false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedViewModel.getStudentDetail()
     }
 }
