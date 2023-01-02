7d0f3c6c86bf7dd4a47a4eb5ce6d8618cf5bf03b
Anirban Basak <jeetbasak2002@gmail.com>
2023-01-02 15:47:04 +0530

    Delete Constants.kt

--- app/src/main/java/com/example/teacherreview/utils/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.teacherreview.utils
-
-class Constants {
-    companion object{
-        const val BASE_URL= "http://192.168.1.205:3030/"
-        const val TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2NzI2MDY0ODYsImV4cCI6MTY3MjY5Mjg4NiwiYXVkIjoiaHR0cHM6Ly95b3VyZG9tYWluLmNvbSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiNjNiMWYyZTY0NGI4MWJjZDQ5NDBkMThkIiwianRpIjoiZWYwOTk3MWQtNzM4OC00YjNmLWE4YzAtNmQyMDk1YTlhYmJkIn0.OYh8fiivjG956RAZ0SrmykFIBnnmACenWSS7CYVXtcs"
-    }
-}
